89e987b22ace5d53fdeb4aba934747a11f7b24a8
Kakurady <kakurady@gmail.com>
2012-12-29 10:22:58 -0500

    variable-width text display as of 2012-09-03

--- varitext_make_texture.py
+++ varitext_make_texture.py
@@ -9,6 +9,9 @@ def python_xytext(font="Tahoma",color=(255,255,255),size=50,
     img.add_layer(layer, 0)
     gimp.set_foreground(color)
     
+    f_w, f_h, f_asc, f_desc = pdb.gimp_text_get_extents_fontname(chars,size,PIXELS,font)
+    #print f_w, f_h, f_asc, f_desc
+    
     extents = []
     try:
         for row in range(rows):
@@ -16,13 +19,18 @@ def python_xytext(font="Tahoma",color=(255,255,255),size=50,
                 index = row * columns + col
                 c_w, c_h, c_asc, c_desc = pdb.gimp_text_get_extents_fontname(chars[index],size,PIXELS,font)
                 extents.append(c_w)
-                y=(row)*(height/rows)-0
-                x=(col+0.5)*(width/columns)-c_w/2
+                y=(row+0.5)*(height/rows)- f_h/2
+                x=(col+0.5)*(width/columns)- c_w/2
+                #print repr(chars[index]), c_w, c_h, c_asc, c_desc
                 pdb.gimp_text_fontname(img,layer,x,y,chars[index],0,TRUE,size,PIXELS,font)
     except IndexError:
         pass
     
-    #pdb.gimp_image_merge_visible_layers(img, 1)
+    merged = pdb.gimp_image_merge_visible_layers(img, 1)
+    mask = pdb.gimp_layer_create_mask(merged, ADD_ALPHA_MASK)
+    pdb.gimp_layer_add_mask(merged, mask)
+    merged.fill(WHITE_FILL)
+    
     gimp.Display(img)
     print extents
 
